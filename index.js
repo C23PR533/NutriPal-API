c9ad9d825083898fd052cf5bb8748c924488fcd5
Razin Syakib <muhammadrazin14@gmail.com>
2023-05-23 22:46:22 +0700

    basic server and depedencies

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+const PORT = 8080;
+
+app.use(bodyParser.json());
+
+app.listen(PORT, () =>
+  console.log(`Server berjalan di ${PORT}`)
+);
