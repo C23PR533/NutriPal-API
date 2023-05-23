d4c82743a2a7b0ec1a94f26a183c7ec148ea6ffc
Razin Syakib <muhammadrazin14@gmail.com>
2023-05-24 01:39:17 +0700

    make PATCH method

--- routes/user_preferences.js
+++ routes/user_preferences.js
@@ -21,10 +21,42 @@ router.get("/", (req, res) => {
 });
 
 router.post("/", (req, res) => {
-    upreferences.push(req.body);
-    writeFileData();
+  upreferences.push(req.body);
+  writeFileData();
 
   res.send(`Saved`);
 });
+
+router.patch("/:id", (req, res) => {
+  const { id } = req.params;
+  const {
+    goals,
+    height,
+    weight,
+    gender,
+    birthdate,
+    activityLevel,
+    disease,
+    favoriteFood,
+  } = req.body;
+
+  const upreference = upreferences.find(
+    (upreference) => upreference.id_user === id
+  );
+
+  if (goals) upreference.goals = goals;
+  if (height) upreference.height = height;
+  if (weight) upreference.weight = weight;
+  if (gender) upreference.gender = gender;
+  if (birthdate) upreference.birthdate = birthdate;
+  if (activityLevel) upreference.activityLevel = activityLevel;
+  if (disease) upreference.disease = disease;
+  if (favoriteFood) upreference.favoriteFood = favoriteFood;
+
+  writeFileData();
+
+  res.send(`Updated id ${id}`);
+});
+
 
 module.exports = router;
