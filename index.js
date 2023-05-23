9b288d90b45d1f2a6533c22fb9ece50fd78e6748
Razin Syakib <muhammadrazin14@gmail.com>
2023-05-23 23:36:12 +0700

    routing rule in index.js and make the routes file

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const dataDiriRoutes = require("./routes/data_diri");
+const userPreference = require("./routes/user_preferences");
+
 const app = express();
 const PORT = 8080;
 
 app.use(bodyParser.json());
+
+app.get("/datadiri", dataDiriRoutes);
+app.get("/userpreferences", userPreference);
 
 app.listen(PORT, () =>
   console.log(`Server berjalan di ${PORT}`)
