3b74fea79191642952bbb3b7516360de6a097557
Razin Syakib <muhammadrazin14@gmail.com>
2023-05-25 14:27:40 +0700

    firestore config and can post the data (just from the index)
    also install firebase, firebase-admin, and cors
    
    npm install --save firebase-admin
    npm install firebase-admin --save
    npm install firebase
     npm install cors@2.8.5 --save

--- index.js
+++ index.js
@@ -5,12 +5,49 @@ const dataDiriRoutes = require("./routes/data_diri");
 const userPreference = require("./routes/user_preferences");
 
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
-app.use(bodyParser.json());
+const { initializeApp } = require("firebase-admin/app");
+
+const { Firestore } = require("@google-cloud/firestore");
+var admin = require("firebase-admin");
+
+var serviceAccount = require("./key.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
 
 app.use("/datadiri", dataDiriRoutes);
 app.use("/userpreferences", userPreference);
+
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+
+const db = admin.firestore();
+
+app.post("/create", async (req, res) => {
+  try {
+    const id = req.body.id_user;
+    const userJson = {
+      id_user: req.body.id_user,
+      goals: req.body.goals,
+      height: req.body.height,
+      weight: req.body.weight,
+      gender: req.body.gender,
+      birthdate: req.body.birthdate,
+      activityLevel: req.body.activityLevel,
+      disease: req.body.disease || [],
+      favoriteFood: req.body.favoriteFood || [],
+    };
+    const response = db.collection("userPreferences").doc(id).set(userJson);
+    res.send(response);
+  } catch (error) {
+    console.log(error);
+    res.send(error);
+  }
+});
 
 app.listen(PORT, () =>
   console.log(`Server berjalan di ${PORT}`)
--- routes/user_preferences.js
+++ routes/user_preferences.js
@@ -1,68 +1,66 @@
 const express = require("express");
 const router = express.Router();
-const fs = require("fs");
+// const fs = require("fs");
 
-let upreferences = [];
-
-const readFileData = () => {
-  const data = fs.readFileSync("./data/user_preferences.json", "utf8");
-  upreferences = JSON.parse(data);
-};
-
-readFileData();
-
-const writeFileData = () => {
-  const data = JSON.stringify(upreferences, null, 2);
-  fs.writeFileSync("./data/user_preferences.json", data, "utf8");
-};
-
-router.get("/", (req, res) => {
-  res.send(upreferences);
+router.post("/", async (req, res) => {
+  try {
+    const id = req.body.id_user;
+    const userJson = {
+      id_user: req.body.id_user,
+      goals: req.body.goals,
+      height: req.body.height,
+      weight: req.body.weight,
+      gender: req.body.gender,
+      birthdate: req.body.birthdate,
+      activityLevel: req.body.activityLevel,
+      disease: req.body.disease || [],
+      favoriteFood: req.body.favoriteFood || [],
+    };
+    const response = db.collection("userPreferences").doc(id).set(userJson);
+    res.send(response);
+  } catch (error) {
+    console.log(error);
+    res.send(error);
+  }
 });
 
-router.post("/", (req, res) => {
-  upreferences.push(req.body);
-  writeFileData();
 
-  res.send(`Saved`);
-});
+// router.patch("/:id", (req, res) => {
+//   const { id } = req.params;
+//   const {
+//     goals,
+//     height,
+//     weight,
+//     gender,
+//     birthdate,
+//     activityLevel,
+//     disease,
+//     favoriteFood,
+//   } = req.body;
 
-router.patch("/:id", (req, res) => {
-  const { id } = req.params;
-  const {
-    goals,
-    height,
-    weight,
-    gender,
-    birthdate,
-    activityLevel,
-    disease,
-    favoriteFood,
-  } = req.body;
+//   const upreference = upreferences.find((upreference) => upreference.id_user === id);
 
-  const upreference = upreferences.find((upreference) => upreference.id_user === id);
+//   if (goals) upreference.goals = goals;
+//   if (height) upreference.height = height;
+//   if (weight) upreference.weight = weight;
+//   if (gender) upreference.gender = gender;
+//   if (birthdate) upreference.birthdate = birthdate;
+//   if (activityLevel) upreference.activityLevel = activityLevel;
+//   if (disease) upreference.disease = disease;
+//   if (favoriteFood) upreference.favoriteFood = favoriteFood;
 
-  if (goals) upreference.goals = goals;
-  if (height) upreference.height = height;
-  if (weight) upreference.weight = weight;
-  if (gender) upreference.gender = gender;
-  if (birthdate) upreference.birthdate = birthdate;
-  if (activityLevel) upreference.activityLevel = activityLevel;
-  if (disease) upreference.disease = disease;
-  if (favoriteFood) upreference.favoriteFood = favoriteFood;
+//   writeFileData();
 
-  writeFileData();
+//   res.send(`Updated id ${id}`);
+// });
 
-  res.send(`Updated id ${id}`);
-});
+// router.delete("/:id", (req, res) => {
+//   const { id } = req.params;
+//   upreferences = upreferences.filter((upreference) => upreference.id_user !== id);
 
-router.delete("/:id", (req, res) => {
-  const { id } = req.params;
-  upreferences = upreferences.filter((upreference) => upreference.id_user !== id);
+//   writeFileData();
 
-  writeFileData();
-
-  res.send(`Deleted id ${id}`);
-});
+//   res.send(`Deleted id ${id}`);
+// });
 
 module.exports = router;
