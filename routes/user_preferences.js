9ba11a7df54be4511a2689ff70eeb453489e326c
Razin Syakib <muhammadrazin14@gmail.com>
2023-05-25 20:31:42 +0700

    get method (all and by id)

--- routes/user_preferences.js
+++ routes/user_preferences.js
@@ -26,4 +26,32 @@ router.post("/", async (req, res) => {
   }
 });
 
+router.get("/", async (req, res) => {
+  try {
+      const userpredb = db.collection("userPreferences");
+      const response = await userpredb.get();
+      let responseArr = [];
+      response.forEach(doc => {
+        responseArr.push(doc.data());
+      });
+      res.send(responseArr);
+  } catch (error) {
+    console.log(error);
+    res.send(error);
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const userpredb = db.collection("userPreferences").doc(req.params.id);
+    const response = await userpredb.get(userpredb);
+    res.send(response.data());
+  } catch (error) {
+    console.log(error);
+    res.send(error);
+  }
+});
+
+
+
 module.exports = router;
