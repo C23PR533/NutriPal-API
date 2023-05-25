a1675f99e82157e4087545ee24205337faf68aa0
Razin Syakib <muhammadrazin14@gmail.com>
2023-05-25 16:03:35 +0700

    can POST to firestore db (now in /userpreferences)
    Added:
    npm install @google-cloud/firestore
    npm install dotenv

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const express = require("express");
 const bodyParser = require("body-parser");
 
@@ -5,9 +7,14 @@ const dataDiriRoutes = require("./routes/data_diri");
 const userPreference = require("./routes/user_preferences");
 
 const app = express();
+const cors = require("cors");
 const PORT = process.env.PORT || 8080;
 
 const { initializeApp } = require("firebase-admin/app");
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 const { Firestore } = require("@google-cloud/firestore");
 var admin = require("firebase-admin");
@@ -18,37 +25,9 @@ admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 
+const db = admin.firestore();
+
 app.use("/datadiri", dataDiriRoutes);
 app.use("/userpreferences", userPreference);
 
-app.use(express.json());
-app.use(bodyParser.json());
-app.use(express.urlencoded({ extended: true }));
-
-const db = admin.firestore();
-
-app.post("/create", async (req, res) => {
-  try {
-    const id = req.body.id_user;
-    const userJson = {
-      id_user: req.body.id_user,
-      goals: req.body.goals,
-      height: req.body.height,
-      weight: req.body.weight,
-      gender: req.body.gender,
-      birthdate: req.body.birthdate,
-      activityLevel: req.body.activityLevel,
-      disease: req.body.disease || [],
-      favoriteFood: req.body.favoriteFood || [],
-    };
-    const response = db.collection("userPreferences").doc(id).set(userJson);
-    res.send(response);
-  } catch (error) {
-    console.log(error);
-    res.send(error);
-  }
-});
-
-app.listen(PORT, () =>
-  console.log(`Server berjalan di ${PORT}`)
-);
+app.listen(PORT, () => console.log(`Server berjalan di ${PORT}`));
--- routes/user_preferences.js
+++ routes/user_preferences.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
-// const fs = require("fs");
+const { Firestore } = require("@google-cloud/firestore");
+
+const db = new Firestore();
 
 router.post("/", async (req, res) => {
   try {
@@ -16,51 +18,12 @@ router.post("/", async (req, res) => {
       disease: req.body.disease || [],
       favoriteFood: req.body.favoriteFood || [],
     };
-    const response = db.collection("userPreferences").doc(id).set(userJson);
+    const response = await db.collection("userPreferences").doc(id).set(userJson);
     res.send(response);
   } catch (error) {
     console.log(error);
     res.send(error);
   }
 });
-
-
-// router.patch("/:id", (req, res) => {
-//   const { id } = req.params;
-//   const {
-//     goals,
-//     height,
-//     weight,
-//     gender,
-//     birthdate,
-//     activityLevel,
-//     disease,
-//     favoriteFood,
-//   } = req.body;
-
-//   const upreference = upreferences.find((upreference) => upreference.id_user === id);
-
-//   if (goals) upreference.goals = goals;
-//   if (height) upreference.height = height;
-//   if (weight) upreference.weight = weight;
-//   if (gender) upreference.gender = gender;
-//   if (birthdate) upreference.birthdate = birthdate;
-//   if (activityLevel) upreference.activityLevel = activityLevel;
-//   if (disease) upreference.disease = disease;
-//   if (favoriteFood) upreference.favoriteFood = favoriteFood;
-
-//   writeFileData();
-
-//   res.send(`Updated id ${id}`);
-// });
-
-// router.delete("/:id", (req, res) => {
-//   const { id } = req.params;
-//   upreferences = upreferences.filter((upreference) => upreference.id_user !== id);
-
-//   writeFileData();
-
-//   res.send(`Deleted id ${id}`);
-// });
 
 module.exports = router;
