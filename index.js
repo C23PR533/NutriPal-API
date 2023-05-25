d9e9f76eab79dccae6689f02a7dab961774c09e8
Razin Syakib <muhammadrazin14@gmail.com>
2023-05-25 21:35:26 +0700

    crud firestore set up
    Added:
    npm install --save firebase-admin
    npm install firebase-admin --save
    npm install firebase
    npm install cors@2.8.5 --save
    npm install @google-cloud/firestore
    npm install dotenv

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
+require("dotenv").config();
+
 const express = require("express");
 const bodyParser = require("body-parser");
 
 const dataDiriRoutes = require("./routes/data_diri");
-// const userPreference = require("./routes/user_preferences");
+const userPreference = require("./routes/user_preferences");
 
 const app = express();
-const PORT = 8080;
+const cors = require("cors");
+const PORT = process.env.PORT || 8080;
 
+const { initializeApp } = require("firebase-admin/app");
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use("/datadiri", dataDiriRoutes);
-// app.use("/userpreferences", userPreference);
+const { Firestore } = require("@google-cloud/firestore");
+var admin = require("firebase-admin");
 
-app.listen(PORT, () =>
-  console.log(`Server berjalan di ${PORT}`)
-);
+var serviceAccount = require("./key.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+const db = admin.firestore();
+
+app.use("/datadiri", dataDiriRoutes);
+app.use("/userpreferences", userPreference);
+
+app.listen(PORT, () => console.log(`Server berjalan di ${PORT}`));
