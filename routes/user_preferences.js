7b332c6da2c0a378b9825ea4ab85af843739c165
Razin Syakib <muhammadrazin14@gmail.com>
2023-06-14 18:40:23 +0700

    try to compare uid as api auth but still not work
    add:
    npm install firebase-functions

--- routes/user_preferences.js
+++ routes/user_preferences.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { Firestore } = require("@google-cloud/firestore");
 const authMiddleware = require("../middleware/authMiddleware");
+const admin = require("firebase-admin");
 
 const db = new Firestore();
 router.use(authMiddleware);
@@ -19,27 +20,59 @@ router.post("/", async (req, res) => {
       gender: req.body.gender,
       birthdate: req.body.birthdate,
       activityLevel: req.body.activityLevel,
-      disease: req.body.disease || [],
-      favoriteFood: req.body.favoriteFood || [],
+      disease: Array.isArray(req.body.disease)
+        ? req.body.disease
+        : [req.body.disease],
+      favoriteFood: Array.isArray(req.body.favoriteFood)
+        ? req.body.favoriteFood
+        : [req.body.favoriteFood],
     };
 
     for (const field in userJson) {
       if (!userJson[field]) {
-        return res.status(401).json({
+        return res.status(400).json({
+          code: 400,
           error: true,
-          message: `${field} harus diisi`,
+          message: `${field} must be filled in`,
         });
       }
     }
 
+    if (userJson.height <= 53 && userJson.weight <= 2.7) {
+      return res.status(400).json({
+        code: 400,
+        error: true,
+        message:
+          "The height must be greater than 53 cm and the weight must be greater than 2.7 kg",
+      });
+    }
+
+    if (userJson.height <= 53) {
+      return res.status(400).json({
+        code: 400,
+        error: true,
+        message: "The height must be greater than 53 cm",
+      });
+    }
+
+    if (userJson.weight <= 2.7) {
+      return res.status(400).json({
+        code: 400,
+        error: true,
+        message: "The weight must be greater than 2.7 kg",
+      });
+    }
+
     await db.collection("userPreferences").doc(id).set(userJson);
     res.status(200).json({
+      code: 200,
       error: false,
-      message: "Data telah ditambahkan",
+      message: "data has been added",
     });
   } catch (error) {
     console.log(error);
     res.status(400).json({
+      code: 400,
       error: true,
       message: error.message,
     });
@@ -55,41 +88,64 @@ router.get("/", async (req, res) => {
       responseArr.push(doc.data());
     });
     res.status(200).json({
+      code: 200,
       error: false,
-      message: "Data berhasil didapatkan",
+      message: "data has been successfully obtained",
       listUserPreferences: responseArr,
     });
   } catch (error) {
     console.log(error);
     res.status(400).json({
+      code: 400,
       error: true,
       message: error.message,
     });
   }
 });
 
-router.get("/:id", async (req, res) => {
+router.get("/:user_id", validateFirebaseUid, async (req, res) => {
   try {
-    const idUserPre = req.params.id;
-    const userpredb = db.collection("userPreferences").doc(req.params.id);
+    const idUserPre = req.params.user_id;
+    const userpredb = db.collection("userPreferences").doc(req.params.user_id);
     const response = await userpredb.get(userpredb);
+
+    // Mendapatkan token akses dari header atau query parameter
+    const token = req.headers.authorization || req.query.token;
+
+    // Verifikasi token akses menggunakan Firebase Admin SDK
+    const decodedToken = await admin.auth().verifyIdToken(token);
+
+    // Mengambil UID pengguna dari token akses yang diverifikasi
+    const uid = decodedToken.uid;
+
+    // Memeriksa apakah UID pengguna sesuai dengan ID pengguna yang diminta
+    if (uid !== idUserPre) {
+      return res.status(401).json({
+        code: 401,
+        error: true,
+        message: "Unauthorized access",
+      });
+    }
 
     if (!response.exists) {
       return res.status(404).json({
+        code: 404,
         error: true,
-        message: `Data User Preference dengan id ${idUserPre} tidak ditemukan`,
+        message: `User Preference data with id ${idUserPre} not found`,
       });
     }
 
     res.status(200).json({
+      code: 200,
       error: false,
-      message: `Data User Preference dengan id ${idUserPre} berhasil didapatkan`,
+      message: `User Preference data with id ${idUserPre} successfully obtained`,
       listUserPreferences: response.data(),
     });
     // res.send(response.data());
   } catch (error) {
     console.log(error);
     res.status(400).json({
+      code: 400,
       error: true,
       message: error.message,
     });
@@ -107,28 +163,60 @@ router.put("/:id", async (req, res) => {
       gender: req.body.gender,
       birthdate: req.body.birthdate,
       activityLevel: req.body.activityLevel,
-      disease: req.body.disease || [],
-      favoriteFood: req.body.favoriteFood || [],
+      disease: Array.isArray(req.body.disease)
+        ? req.body.disease
+        : [req.body.disease],
+      favoriteFood: Array.isArray(req.body.favoriteFood)
+        ? req.body.favoriteFood
+        : [req.body.favoriteFood],
     };
 
     for (const field in userJson) {
       if (!userJson[field]) {
-        return res.status(401).json({
+        return res.status(400).json({
+          code: 400,
           error: true,
-          message: `${field} harus diisi`,
+          message: `${field} must be filled in`,
         });
       }
+    }
+
+    if (userJson.height <= 53 && userJson.weight <= 2.7) {
+      return res.status(400).json({
+        code: 400,
+        error: true,
+        message:
+          "The height must be greater than 53 cm and the weight must be greater than 2.7 kg",
+      });
+    }
+
+    if (userJson.height <= 53) {
+      return res.status(400).json({
+        code: 400,
+        error: true,
+        message: "The height must be greater than 53 cm",
+      });
+    }
+
+    if (userJson.weight <= 2.7) {
+      return res.status(400).json({
+        code: 400,
+        error: true,
+        message: "The weight must be greater than 2.7 kg",
+      });
     }
 
     await db.collection("userPreferences").doc(idParams).update(userJson);
 
     res.status(200).json({
+      code: 200,
       error: false,
-      message: `Data User Preference dengan id ${idParams} telah diupdate`,
+      message: `User Preference data with id ${idParams} has been update`,
     });
   } catch (error) {
     console.log(error);
     res.status(400).json({
+      code: 400,
       error: true,
       message: error.message,
     });
@@ -143,12 +231,14 @@ router.delete("/:id", async (req, res) => {
       .doc(req.params.id)
       .delete();
     res.status(200).json({
+      code: 200,
       error: false,
-      message: `Data User Preference dengan id ${idParams} telah dihapus`,
+      message: `User Preference data with id ${idParams} has been deleted`,
     });
   } catch (error) {
     console.log(error);
     res.status(400).json({
+      code: 400,
       error: true,
       message: error.message,
     });
